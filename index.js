299815fcb87264457d6feb1a33510695c018ee29
darshak767 <darshak767@gmail.com>
2026-08-24 20:34:50 +0530

    Version 1 is done

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
 const chat = require("./models/chat.js");
+const methodoverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodoverride("_method"));
 
 //call the main function for connect database
 main()
@@ -46,15 +48,61 @@ app.post("/chats", (req, res) => {
   });
 
   newchat
-    .save() 
-    .then((res) => {  //if we use then it means it's working automatically.
+    .save()
+    .then((res) => {
+      //if we use then it means it's working automatically.
       console.log("Chat Save sucessfully");
     })
-    .catch((err)=>{
+    .catch((err) => {
       console.log(err);
     });
   res.redirect("/chats");
 });
+
+//Edit Routes
+app.get("/chats/:id/edit", async (req, res) => {
+  let { id } = req.params;
+
+  let chats = await chat.findById(id);
+
+  res.render("edit.ejs", { chats });
+});
+
+app.put("/chats/:id", async (req, res) => {
+  let { id } = req.params;
+
+  let { from, message, to } = req.body;
+
+  // console.log("id :", id);
+  // console.log("Message",message);
+  // console.log("To",to);
+
+  let updatedchat = await chat.findByIdAndUpdate(
+    id,
+    {
+      from: from,
+      message: message,
+      to: to,
+    },
+    { new: true },
+  );
+
+  // console.log("UpdatedChat :", updatedchat);
+
+  res.redirect("/chats");
+});
+
+
+//destroy routes
+app.delete("/chats/:id",async (req, res) =>{
+  let {id} = req.params;
+
+  let deletechat = await chat.findByIdAndDelete(id);
+
+  console.log(deletechat);
+
+  res.redirect("/chats");
+})
 
 app.listen(8000, () => {
   console.log("Server is listing on port 8000");
